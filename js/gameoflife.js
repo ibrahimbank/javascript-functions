531f652f520b5bcef0f1b02c29eca95d81eda53b
bankole <bankoleibrahim96@gmail.com>
2021-11-02 11:31:57 +0100

    solution

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -12,7 +12,11 @@ function same([x, y], [j, k]) {
 }
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) {
+  let cellValue = [cell];
+  if (!cellValue) return;
+  return this.cellValue;
+}
 
 const printCell = (cell, state) => {};
 

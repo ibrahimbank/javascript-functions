0658780351199f955746a457ef74233e420bef07
bankole <bankoleibrahim96@gmail.com>
2021-11-03 19:31:27 +0100

    solution

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -6,7 +6,7 @@ function seed(a, b, c) {
 }
 
 function same([x, y], [j, k]) {
-  return x === y && j === k;
+  return x === j && y === k;
 }
 
 // The game state to search for `cell` is passed as the `this` value of the function.

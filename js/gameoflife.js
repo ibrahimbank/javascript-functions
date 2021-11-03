4bbda80cfbe6b246b5f9c42a61c0775a3589cd4b
bankole <bankoleibrahim96@gmail.com>
2021-11-03 19:52:10 +0100

    solution

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -51,16 +51,16 @@ const printCells = (state) => {
   return acc;
 };
 
-const getNeighborsOf = ([x, y]) => {
+const getNeighborsOf = ([x, y]) => [
   [x - 1, y + 1],
-    [x, y + 1],
-    [x + 1, y + 1],
-    [x - 1, y],
-    [x + 1, y],
-    [x - 1, y - 1],
-    [x, y - 1],
-    [x + 1, y - 1];
-};
+  [x, y + 1],
+  [x + 1, y + 1],
+  [x - 1, y],
+  [x + 1, y],
+  [x - 1, y - 1],
+  [x, y - 1],
+  [x + 1, y - 1],
+];
 
 const getLivingNeighbors = (cell, state) => {
   return getNeighborsOf(cell).filter((n) => contains.bind(state)(n));
